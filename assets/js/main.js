637ce828ad3833e49c8eece35ba6b9f5e85440ce
e5dran <esdran.bc@gmail.com>
2022-10-24 20:50:01 +0200

    fin filtros

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,19 +2,23 @@ const inputTarea = document.getElementById('inputTarea');
 const selectPrioridad = document.getElementById('selectPrioridad');
 const formulario = document.getElementById('formulario');
 
+const inputTareaFilter = document.getElementById('inputTareaFilter');
+const selectPrioridadFilter = document.getElementById('selectPrioridadFilter');
+const btnLimpiar = document.getElementById('btnLimpiar');
+
 const listaTareas = document.getElementById('listaTareas');
 
 let tareas = [];
 if (localStorage.getItem('listTareas')) {
     tareas = JSON.parse(localStorage.getItem('listTareas'))
 };
-pintarLista();
+pintarLista(tareas);
 
 formulario.addEventListener('submit', (event) => {
     event.preventDefault();
     const dato = {
         /* idTarea, */
-        titulo: inputTarea.value,
+        titulo: inputTarea.value.toLowerCase(),
         prioridad: selectPrioridad.value
     }
     tareas.push(dato);
@@ -22,10 +26,32 @@ formulario.addEventListener('submit', (event) => {
 
     inputTarea.value = '';
     selectPrioridad.value = '';
-    pintarLista();
+    pintarLista(tareas);
 })
 
-function pintarLista() {
+selectPrioridadFilter.addEventListener('change', (event) => {
+    event.preventDefault();
+    let tareasFiltro = [];
+    tareasFiltro = tareas.filter(tarea => tarea.prioridad === selectPrioridadFilter.value)
+    pintarLista(tareasFiltro);
+    if (selectPrioridadFilter.value === 'all') {
+        pintarLista(tareas);
+    }
+})
+
+inputTareaFilter.addEventListener('input', (event) => {
+    let tareasFiltro = [];
+    tareasFiltro = tareas.filter(tarea => tarea.titulo === inputTareaFilter.value.toLowerCase())
+    pintarLista(tareasFiltro);
+})
+
+btnLimpiar.addEventListener('click', () => {
+    pintarLista(tareas);
+    selectPrioridadFilter.value = 'all';
+    inputTareaFilter.value = '';
+})
+
+function pintarLista(tareas) {
     listaTareas.innerHTML = '';
     for (let i = 0; i < tareas.length; i++) {
         const li = document.createElement('li');
@@ -43,5 +69,6 @@ function pintarLista() {
         listaTareas.append(li);
     }
 }
+
 
 
